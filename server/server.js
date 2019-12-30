522de58bc5eef2dc166e1d5169a8706acf4513f4
Jason Seidler <jsonseidler@gmail.com>
2019-12-30 16:58:58 -0500

    basic server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/index.html'))
+})
+
+
+
+app.listen(PORT, () => console.log(`The server is listening on PORT ${PORT}`))
