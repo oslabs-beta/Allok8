791afe82a33dd56f2591f3557d46bb26838399d8
Tyler Meyerson <tdmeyerson@gmail.com>
2019-12-30 20:29:03 -0500

    broke and fixed npm start

--- server/server.js
+++ server/server.js
@@ -1,15 +1,38 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+
 const PORT = 3000;
 const app = express();
 const path = require('path');
 
 
-// app.use(express.static('dist'))
+// const xRouter = require('./routes/x');
 
+// app.use(bodyParser.json());
+
+// statically serve everything in the build folder on the route '/build'
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
+
+// root, send index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../src/index.html'))
-})
+  res.sendFile(path.resolve(__dirname, '../src/index.html'));
+});
 
+// catch-all route handler for any requests to an unknown route
+app.all('*', (req, res) => res.status(404).send('Page not found'));
 
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
 
-app.listen(PORT, () => console.log(`The server is listening on PORT ${PORT}`))
+  const errorObj = Object.assign(defaultErr, err);
+  console.log(errorObj.log);
+  res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => console.log(`The server is listening on PORT ${PORT}`));
