b738112c167c28a0629e71815f4bdba3f00a753e
Jason Seidler <jsonseidler@gmail.com>
2020-01-01 17:43:00 -0500

    added node and pod info as middleware

--- server/controllers/curlController.js
+++ server/controllers/curlController.js
@@ -8,18 +8,30 @@ k8.getNodeInfo = (req, res, next) => {
   cmd.get(
     `curl https://${api}/api/v1/nodes?limit=500 --header "Authorization: Bearer ${token}" --insecure`,
     function(err, data, stderr){
+      //error handle if needed
+      if (err) {
+        return next(err);
+      }
         const obj = JSON.parse(data);
         const numOfNodes = obj.items.length;
         const nodeNameArray = [];
         const nodeMetricsRaw = {};
         // console.log(obj.items)
+        //loop through nodes 
         obj.items.forEach(item => {
+          //and add each node to an array
           nodeNameArray.push(item.metadata.name);
+          //keep track of each nodes usage metrics in an object
           nodeMetricsRaw[item.metadata.name] = item.status
         })
+        res.locals.nodeInfo = {
+          nodeNameArray,
+          nodeMetricsRaw
+        };
         // console.log(nodeNameArray)
         // console.log(nodeMetricsRaw)
         // console.log('the current number of nodes is: ', numOfNodes) 
+        return next();
         
     }
   )
@@ -34,17 +46,35 @@ k8.getPodInfo = (req, res, next) => {
   cmd.get(
     `curl https://${api}/api/v1/namespaces/${namespace}/pods?limit=500 --header "Authorization: Bearer ${token}" --insecure`,
     function(err, data, stderr){
+      //error handle if needed
+      if (err) {
+        return next(err);
+      }
       const obj = JSON.parse(data);
       const podNameArray = [];
       const podInfo = {};
       const containerInfo = {};
+      // todo swap this to use object.keys to iterate instead of a for in
+
+      //? there is definitely a better way to do this we will need to revisit how this object looks together
+
       for (let i in obj.items) 
+      //make sure pod is on current node
         if (obj.items[i].metadata.namespace === `${namespace}`) {
+          //push to an array of pod names
           podNameArray.push(obj.items[i].metadata.name);
+          //keep track of each pod in an object
           podInfo[obj.items[i].metadata.name] = obj.items[i]
         }
+      //creates an object that has the information for the containers in the pod as key,value pairs by pod
       podNameArray.forEach(pod => containerInfo[pod] = podInfo[pod].spec.containers )
       // console.log(containerInfo)
+      res.locals.containerInfo = {
+        containerInfo,
+        podInfo,
+        podNameArray
+      }
+      return next();
     }
   )
 }
