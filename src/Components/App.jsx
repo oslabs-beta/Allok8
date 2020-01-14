de7ad036b9f591b9c69450321a81b6c2d138daa6
Tyler Meyerson <tdmeyerson@gmail.com>
2020-01-13 20:47:10 -0500

    Undid router on App, caused problems with fetching data

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,42 +2,38 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
 import Landing from './Landing.jsx';
 import Dashboard from './Dashboard.jsx';
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
+import LineChart from './LineChart.jsx';
+import Sunburst from './SunburstChart.jsx';
+import InfoPane from './InfoPanel.jsx';
 import DisplayPane from './DisplayPane.jsx';
 
 
 const mapStateToProps = (store) => ({
-  // page: store.state.page,
+  page: store.state.page,
   // payload: store.state.payload,
 });
 
 class App extends Component {
   // renders a button and a boolean; the button flips the boolean
   render() {
-    return (
-      <Router>
-        <div className="max">
-          <Header />
-          <Switch>
-            <Route exact path="/">
-              <Landing />
-            </Route>
-            <Route path="/dashboard">
-              <Dashboard />
-            </Route>
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    );
+    switch (this.props.page) {
+      case 'DASHBOARD':
+        console.log('Dashboard');
+        return (
+          <div>
+            {/* <InfoPane /> */}
+            <Sunburst />
+            {/* <LineChart /> */}
+          </div>
+        );
+      case 'LANDING':
+        console.log('Landing');
+        return (<Landing />);
+      default:
+        return (<h1>ERROR</h1>);
+    }
   }
 }
 
