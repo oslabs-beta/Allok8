7e80a62479254ba855a27d39cb838680283f4ccf
Rex Osariemen <rexosariemen@gmail.com>
2020-01-15 21:58:04 -0500

    fix multiple node display

--- src/Components/Tree.jsx
+++ src/Components/Tree.jsx
@@ -2,65 +2,6 @@ import React, { Component } from 'react';
 import Tree from 'react-d3-tree';
 import { connect } from 'react-redux';
 
-// console.log(myTreeData);
-
-// const myTreeData = [
-//   {
-//     name: 'Top Level',
-//     attributes: {
-//       keyA: 'val A',
-//       keyB: 'val B',
-//       keyC: 'val C',
-//     },
-//     children: [
-//       {
-//         name: 'Level 2: A',
-//         attributes: {
-//           keyA: 'val A',
-//           keyB: 'val B',
-//           keyC: 'val C',
-//         },
-//         children: [
-//           {
-//             name: 'Level 3: C',
-//             attributes: {
-//               propA: 'Val A',
-//               propB: 'Val B'
-//             },
-//             children: [
-//               {
-//                 name: 'Level 4: D',
-//                 attributes: {
-//                   propA: 'Val A',
-//                 }
-//               },
-//               {
-//                 name: 'Level 4: D',
-//                 attributes: {
-//                   propA: 'val A',
-//                   propB: 'val B'
-//                 }
-//               }
-//             ]
-//           },
-//           {
-//             name: 'Level 3: C',
-//             attributes: {
-//               propA: 'Val A',
-//               propB: 'Val B'
-//             }
-//           }
-//         ]
-//       },
-//       {
-//         name: 'Level 2: B',
-//         attributes: {
-//           pro: 'newAtt'
-//         }
-//       },
-//     ],
-//   },
-// ];
 
 const mapStateToProps = (store) => ({
   data: store.state.data
