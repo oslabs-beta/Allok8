32270b7faa671e1aeaf9ae5a3999ffc855b0322d
Tyler Meyerson <tdmeyerson@gmail.com>
2020-01-15 23:56:45 -0500

    Update App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -6,7 +6,6 @@ import Landing from './Landing.jsx';
 import Dashboard from './Dashboard.jsx';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
-import TreeComponent from './Tree.jsx';
 
 
 const mapStateToProps = (store) => ({
