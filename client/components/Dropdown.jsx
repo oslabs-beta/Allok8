daf727e2df5cb1155839040d7c0f3493865fda28
Alec Below <abelow@umich.edu>
2020-04-29 14:28:23 -0400

    implemented setstate in dropdown

--- client/components/Dropdown.jsx
+++ client/components/Dropdown.jsx
@@ -4,11 +4,15 @@ const Dropdown = (props) => {
   return (
     <div className="dataSelector">
       <label htmlFor="data">Select data to chart:</label>
-      <select id="data">
-        <option value="memoryPercent">Memory Percent</option>
-        <option value="memoryUsed">Memory Used</option>
-        <option value="cpuPercent">CPU Percent</option>
-        <option value="cpuUsed">CPU Used</option>
+      <select
+        id="data"
+        defaultValue={props.method}
+        onChange={(e) => props.setMethod(e.target.value)}
+      >
+        <option value="memory_percent">Memory Percent</option>
+        <option value="memory_used">Memory Used</option>
+        <option value="cpu_percent">CPU Percent</option>
+        <option value="cpu_used">CPU Used</option>
       </select>
     </div>
   );
