102b4af82d2006586e6f916049a01b3f2107feff
rbargues <rbargues@gmail.com>
2020-04-29 21:24:19 -0400

    finalized containerization of application

--- server/models/dbmodel.js
+++ server/models/dbmodel.js
@@ -6,7 +6,7 @@ const PG_URI = TEST_URI;//process.env.NODE_ENV === 'test' ? TEST_URI : GEN_URI;
 
 const pool = new Pool({
   //this is alec i put this here for testing
-  connectionString: "postgres://postgres:postgres@192.168.99.100:30712/postgres",
+  connectionString: "postgres://postgres:postgres@postgres/postgres",
 });
 
 module.exports = {
